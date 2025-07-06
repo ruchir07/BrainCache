600f485c2322eb77fe644406c43296aa45398b10
Ruchir <ruchir.dhanawade@gmail.com>
2025-07-06 17:08:43 +0530

    Change 23

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -6,7 +6,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: './',
   plugins: [react(),tailwindcss()],
   build: {
     outDir: 'dist',
